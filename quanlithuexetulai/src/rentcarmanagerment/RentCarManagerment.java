495bae9f94a94c906141144b166f957e7a04efa7
nptran <nptran9810@gmail.com>
2019-05-02 17:20:33 +0100

    build connection to db

--- quanlithuexetulai/src/rentcarmanagerment/RentCarManagerment.java
+++ quanlithuexetulai/src/rentcarmanagerment/RentCarManagerment.java
@@ -1,5 +1,12 @@
 package rentcarmanagerment;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Tran Phuc
@@ -10,7 +17,24 @@ public class RentCarManagerment {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Connection conn = null;
+        try {
+            Class.forName("org.h2.Driver");
+            String dbUrl = "jdbc:h2:~/test";
+            conn = DriverManager.getConnection(dbUrl, "test", "test");
+            
+            System.out.println("Connected to database!");
+        } catch (SQLException ex) {
+            Logger.getLogger(RentCarManagerment.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(RentCarManagerment.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(RentCarManagerment.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
 }
